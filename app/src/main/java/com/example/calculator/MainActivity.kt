9fee752b1eb30ae2e59aee38aba2ef8153b87ed0
kj10dev <kj10dev@gmail.com>
2023-01-18 22:04:18 +0200

    Fragments complete
    BottomNavView complete
    Fragments complete
    Basic functionality

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -4,17 +4,29 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import com.example.calculator.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    //activity viewbinding
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        val view = binding.root
+        setContentView(view)
+
+        //setup toolbar
+        val toolbar = binding.toolbar
+        setSupportActionBar(toolbar)
 
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
-        val bottomNavView = findViewById<BottomNavigationView>(R.id.bottom_nav)
-        bottomNavView.setupWithNavController(navController)
+
+        //setup bottomNavView
+        binding.bottomNav.setupWithNavController(navController)
     }
 }
--- app/src/main/java/com/example/calculator/ui/SubtractionFragment.kt
+++ app/src/main/java/com/example/calculator/ui/SubtractionFragment.kt
@@ -5,16 +5,41 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.example.calculator.R
+import com.example.calculator.data.CalculatorViewModel
+import com.example.calculator.databinding.FragmentAdditionBinding
+import com.example.calculator.databinding.FragmentSubtractionBinding
 
 class SubtractionFragment : Fragment() {
+
+    //required for viewBinding
+    private var _binding: FragmentSubtractionBinding? = null
+    private val binding get() = _binding!!
+
+    //required for viewModel class CalculatorViewModel
+    lateinit var viewModel: CalculatorViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_subtraction, container, false)
+        _binding = FragmentSubtractionBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        //setup the viewModel
+        viewModel = ViewModelProvider(this).get(CalculatorViewModel::class.java)
+
+        //minus button
+        binding.fabMinus.setOnClickListener(){
+            binding.answer.text = viewModel.subtraction(
+                binding.firstNum.text.toString().toInt(),
+                binding.secNum.text.toString().toInt()
+            ).toString()
+        }
+
+        return view
     }
 
 }
